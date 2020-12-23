1a5da485a3773430a8e65f52f59771cc789eb352
isidoromendez <isidoro.mendez@gmail.com>
2020-12-23 15:55:46 -0300

    checkpoint

--- src/app/components/pages/lanin/lanin.component.ts
+++ src/app/components/pages/lanin/lanin.component.ts
@@ -14,17 +14,52 @@ export class LaninComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.positionTheDot();
+    this.drawLine( document.getElementById('#bx_a'),document.getElementById('path') );
   }
 
   public toolbarClass = "transp";
   @HostListener('window:scroll') onScroll(e: Event): void {
       let top = document.documentElement.scrollTop
-      if(top > 200){
-        this.toolbarClass = "fondo";        
-      }else{
-        this.toolbarClass = "transp"; 
-      }
-      console.log(this.toolbarClass);
+      this.setToolbarClass(top);
+      this.positionTheDot();
+      this.drawLine( document.getElementById('#bx_a'),document.getElementById('path') );
+  }
+
+  //draw the line
+  public drawLine(container, line) {
+    var pathLength = line.getTotalLength(),
+    maxScrollTop =  document.documentElement.scrollHeight - window.innerHeight,
+    percentDone = (document.documentElement.scrollTop - 250) / maxScrollTop,
+    length = percentDone * pathLength * 2;
+    line.style.strokeDasharray = [ length,pathLength].join(' ');
+  }
+
+  public positionTheDot(){
+    // What percentage down the page are we? 
+    
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop -250) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    // Get path length
+    var path:any = document.getElementById("path");
+    var pathLen = path.getTotalLength();
+    // Get the position of a point at <scrollPercentage> along the path.
+    var pt = path.getPointAtLength(scrollPercentage * pathLen * 2);
+    
+    // Position the red dot at this point
+    var dot = document.getElementById("dot");		  
+    var x = pt.x - 400;
+    var y = (pt.y+10);
+    dot.setAttribute("transform", "translate("+ x + "," + y + ")");
+    console.log(x,pt.y);
+  }
+
+  public setToolbarClass(top: number){
+    if(top > 200){
+      this.toolbarClass = "fondo";        
+    }else{
+      this.toolbarClass = "transp"; 
+    }
+    console.log(this.toolbarClass);
   }
 
   getYPosition(e: Event): number {
